25f4a2affd3da1122d7939d7b1398663df38c6cc
Raghuram7777 <166889309+Raghuram7777@users.noreply.github.com>
2025-01-21 12:03:17 +0530

    Time: 0 ms (100%), Space: 7.6 MB (85.17%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.c
+++ 1013-fibonacci-number/1013-fibonacci-number.c
@@ -1,11 +1,15 @@
-int f(int n,int* dp){
-    if(n==0||n==1){
-        return n;
+int f(int n){
+    if(n==0||n==1)return n;
+    int p1=0;
+    int p2=1;
+    int fibo=0;
+    for(int i=2;i<=n;i++){
+        fibo=p1+p2;
+        p1=p2;
+        p2=fibo;
     }
-    dp[n]=f(n-1,dp)+f(n-2,dp);
-    return dp[n];
+    return fibo;
 }
 int fib(int n){
-    int *dp=(int *)malloc((sizeof(int))*(n+1));
-    return f(n,dp);
+    return f(n);
 }
