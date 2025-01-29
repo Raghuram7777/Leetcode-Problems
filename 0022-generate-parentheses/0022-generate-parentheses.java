75051e02bb664c524275eeefe166a662546b9ae0
Raghuram7777 <166889309+Raghuram7777@users.noreply.github.com>
2025-01-29 16:00:16 +0530

    Time: 11 ms (5.18%), Space: 44.9 MB (6.73%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean valid(String s){
+        int c=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='(')c++;
+            else c--;
+            if(c<0)return false;
+        }
+        return c==0;
+    }
+    public void solve(StringBuilder sb,List<String> l,int n,int i){
+        if(i==2*n){ 
+            if(valid(sb.toString())==true){
+            l.add(sb.toString());}
+            return;
+        }
+        sb.append('(');
+        solve(sb,l,n,i+1);
+        sb.deleteCharAt(sb.length()-1);
+        sb.append(')');
+        solve(sb,l,n,i+1);
+        sb.deleteCharAt(sb.length()-1);
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> l=new ArrayList<>();
+        solve(new StringBuilder(""),l,n,0);
+        return l;
+    }
+}
